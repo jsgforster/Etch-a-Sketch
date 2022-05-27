943b212667bffaa43b1b837492f3973968c8bb3d
Joshua Forster <jsg.forster@googlemail.com>
2022-05-27 16:31:35 +0700

    Add instructions modal

--- script.js
+++ script.js
@@ -239,3 +239,28 @@ color_picker.onchange = function() {
 }
 color_picker_wrapper1.style.backgroundColor = color_picker1.value;
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
